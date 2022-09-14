fb90faa4298e2052e23bd6b56f6c73b46d282e14
dmitriy.martynenko <dmitriy.martynenko@maxpay.com>
2022-09-14 11:23:53 +0300

    initial

--- src/main/java/config/RunnerConfig.java
+++ src/main/java/config/RunnerConfig.java
@@ -31,5 +31,15 @@ public class RunnerConfig {
                 .screenshots(true)
                 .savePageSource(true));
         CustomLogger.logger.info("ok");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+
+        Configuration.browserCapabilities.merge(options);
     }
 }
