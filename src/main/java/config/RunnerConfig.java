1aa9546d9a1c03a1cf6eb10af0a921cbc0c221ab
dmitriy.martynenko <dmitriy.martynenko@maxpay.com>
2022-09-14 12:39:55 +0300

    initial

--- src/main/java/config/RunnerConfig.java
+++ src/main/java/config/RunnerConfig.java
@@ -39,7 +39,7 @@ public class RunnerConfig {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
 
         Configuration.browserCapabilities.merge(options);
     }
